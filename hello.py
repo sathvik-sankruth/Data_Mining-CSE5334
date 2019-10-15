1de22a5ccadae08b39201713508b55f9b20bb222
Sathvik Sankruth <sankruth06@gmail.com>
2019-10-15 02:12:37 -0500

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, url_for, request
+import video_engine as ii
+import os
+app = Flask(__name__)
+
+
+#@app.before_first_request
+#def tdm_generator():
+  #if(os.path.isfile('drugDict.dict') and os.path.isfile('drugCorpus.mm')  == False):
+  #  tdm.gen_tdm()
+  #if(os.path.isfile('videodata.pickel') == False):
+    #ii()
+
+
+@app.route('/')
+def home():
+    return render_template('hello.html')
+
+
+@app.route('/result', methods = ['POST','GET'])
+def result():
+  if request.method == 'POST':
+    query = request.form['search']
+    #simmat = be.similarity(str(query))
+    #result = be.topk(simmat)
+    result = ii.matching_score(20,str(query))
+    #print(type(result))
+    if type(result) != str:
+        #print("in html")
+        def path_to_image_html(path):
+            return '<img src="' + path + '" width="120" >'
+        return render_template("hello.html", query=query, result=result.to_html(escape=False,formatters=dict(image=path_to_image_html)))
+    else:
+      return render_template("hello.html", query=query, result=result)
+
+if __name__=="__main__":
+    app.run(debug=False)
